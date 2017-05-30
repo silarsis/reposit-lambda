5d341009f7a4fa519173abf0fca6c474ca4ca59c
Kevin Littlejohn <kevin@littlejohn.id.au>
2017-05-31 07:21:29 +1000

    zipping using python

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 # pip install all the things
 pip install git+https://github.com/silarsis/rp-python-client -t reposit
 # zip up the dir
-cd reposit && zip -r ../reposit.zip .
+cd reposit && python -m zipfile -c ../reposit.zip . && cd ..
 # push the zipped dir to S3
 # deploy from S3 to the lambda
