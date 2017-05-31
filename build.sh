975f03271e513d3ffd13cbf33f6f45c484997dde
Kevin Littlejohn <kevin@littlejohn.id.au>
2017-05-31 21:35:54 +1000

    remove debug

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 # pip install all the things
 pip install git+https://github.com/silarsis/rp-python-client -t reposit
-find / -iname boto3
 # zip up the dir
 cd reposit && python -m zipfile -c ../reposit.zip . && cd ..
 # push the zipped dir to S3
