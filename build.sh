8a64ffe0f100b9a1dd745d40a11cbe338c86c265
Kevin Littlejohn <kevin@littlejohn.id.au>
2017-05-31 21:21:16 +1000

    need boto3, apparently not installed

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # pip install all the things
 pip install git+https://github.com/silarsis/rp-python-client -t reposit
+pip install boto3
 # zip up the dir
 cd reposit && python -m zipfile -c ../reposit.zip . && cd ..
 # push the zipped dir to S3
